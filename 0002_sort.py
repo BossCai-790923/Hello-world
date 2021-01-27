c5016aeeb1b4053029b06819f99b046b8055a251
BossCai-790923 <78028431+BossCai-790923@users.noreply.github.com>
2021-01-27 17:52:44 +0800

    Add files via upload

---
+++ 0002_sort.py
@@ -0,0 +1,6 @@
+l=[]
+for i in range(5):
+    x=input('add a number: ')
+    l.append(x)
+l.sort()
+print(l)
---
+++ 0003_dictionary.py
@@ -0,0 +1,4 @@
+# China
+phone_numbers={'boss':'15618312602','leo':'15021008678'}
+x=input('who"s phone number : ')
+print(phone_numbers[x])
